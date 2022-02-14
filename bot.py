28d47298dacd542282fb7d22fe02016f333c1e87
Viacheslav <kuzneckiy@icloud.com>
2022-02-14 12:35:22 +0300

    New shiny bot

---
+++ bot.py
@@ -0,0 +1,31 @@
+from distutils.log import log
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+import Settings
+
+logging.basicConfig(filename='bot.log', level=logging.INFO)
+
+
+def greet_user(update, context):
+    print('Вызван /start')
+    update.message.reply_text('Привет')
+    
+def talk_to_me(update, context):
+    text = update.message.text
+    print(text)
+    update.message.reply_text(text)
+
+def main():
+    mybot = Updater(Settings.API_KEY, use_context=True)
+    
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+    logging.info('Бот стартовал')
+    mybot.start_polling()
+    mybot.idle()
+if __name__ == '__main__':
+    main()
+
